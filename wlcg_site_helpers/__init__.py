bc8ffc5727c822b8a2e52cf551c29d6aaf9ae6be
kreczko <lkreczko@gmail.com>
2018-07-12 14:52:30 +0100

    added logging for whole package

--- wlcg_site_helpers/__init__.py
+++ wlcg_site_helpers/__init__.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
-
+import logging
 """Top-level package for wlcg-site-helpers."""
 
 __author__ = """Luke Kreczko"""
 __email__ = 'kreczko@cern.ch'
 __version__ = '0.0.1'
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
