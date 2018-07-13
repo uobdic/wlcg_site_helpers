55de9a2a9b90b14545e880774d51a30de03382d9
kreczko <lkreczko@gmail.com>
2018-07-13 15:09:41 +0100

    modified storage consistency to work with the new HDFS and DAVIX outputs

--- wlcg_site_helpers/storage/consistency.py
+++ wlcg_site_helpers/storage/consistency.py
@@ -5,41 +5,75 @@ from __future__ import absolute_import, print_function
 import gfal2
 import os
 import logging
-import hdfs
-import grid
 import json
+from .backend import hdfs
+from .backend import davix
+import pandas as pd
 
+logger = logging.getLogger(__name__)
 
-def get_folders(prefix, fs = hdfs, cache='localfs.json'):
+def get_folders(prefix, ls_func = hdfs.ls, cache='localfs.csv'):
     if os.path.exists(cache):
+        logger.info('Found cache file {0} ... loading data'.format(cache))
         with open(cache) as f:
-            return json.load(f)
-    folders = list(remove_prefix(fs.ls(prefix, True), prefix))
-    with open(cache, 'w') as f:
-        json.dump(folders, f)
-    return folders
+            return pd.read_csv(f, index_col=0)
 
+    logger.info('Writing results to cache ({0})'.format(cache))
+    df = ls_func(prefix, True)
+    df.to_csv(cache)
+    return df
 
-def remove_prefix(paths, prefix):
-    for p in paths:
-        yield p.replace(prefix, '')
+def get_cache_files(cache_path):
+    if not os.path.exists(cache_path):
+        os.makedirs(cache_path)
+    
+    cache_files = ['local_entries.csv', 'grid_entries.csv', 'local_but_not_grid.csv', 'grid_but_not_local.csv']
+    return [os.path.join(cache_path, f) for f in cache_files]
 
+def local_entries(local_path, ls_func, cache_path='/tmp'):
+    cache_local_entries, _, _, _ = get_cache_files(cache_path)
+    entries_local = get_folders(local_path, ls_func, cache_local_entries)
+    return entries_local
 
-if __name__ == '__main__':
-    prefix_grid = 'davs://lcgse01.phy.bris.ac.uk/dpm/phy.bris.ac.uk/home/cms/store'
-    prefix_localfs = '/dpm/phy.bris.ac.uk/home/cms/store'
+def grid_entries(grid_path, ls_func, cache_path='/tmp'):
+    _, cache_grid_entries, _, _ = get_cache_files(cache_path)
+    entries_grid = get_folders(grid_path, ls_func, cache_grid_entries)
+    return entries_grid
 
-    folders_hdfs = get_folders(prefix_localfs, fs=hdfs, cache='localfs.json')
-    folders_grid = get_folders(prefix_grid, fs=grid, cache='grid.json')
+def get_diff(df1, df2, cache='diff.csv'):
+    if os.path.exists(cache):
+        logger.info('Found cache file {0} ... loading data'.format(cache))
+        with open(cache) as f:
+            return pd.read_csv(f, index_col=0)
 
-    set_hdfs = set(folders_hdfs)
-    set_grid = set(folders_grid)
-    hdfs_but_not_grid = list(set_hdfs - set_grid)
-    print('N items on HDFS but not on grid', len(hdfs_but_not_grid), '\n', '\n'.join(hdfs_but_not_grid[:5]))
-    with open('hdfs_but_not_grid.json', 'w') as f:
-        json.dump(hdfs_but_not_grid, f)
+    df = pd.merge(df1, df2, how='outer', indicator=True, on='path')
+    df1_only = df[df._merge == 'left_only']
+    df1_only = df1_only.rename(columns={'size_x':'size', 'date_x':'date', 'ftype_x':'ftype'})
+    df1_only = df1_only.drop(['size_y', 'date_y', 'ftype_y', '_merge'], axis=1)
+    logger.info('Writing results to cache ({0})'.format(cache))
+    df1_only.to_csv(cache)
 
-    grid_but_not_hdfs = list(set_grid - set_hdfs)
-    print('N items on grid but not on hdfs', len(grid_but_not_hdfs), '\n', '\n'.join(grid_but_not_hdfs[:5]))
-    with open('grid_but_not_hdfs.json', 'w') as f:
-        json.dump(grid_but_not_hdfs, f)
+    return df1_only
+
+def diff_local_but_not_grid(entries_grid, entries_local, cache_path='/tmp'):
+    _, _, cache_diff_local_grid, _ = get_cache_files(cache_path)
+    local_but_not_grid = get_diff(entries_local, entries_grid, cache_diff_local_grid)
+    msg = '{0} items on local FS but not on grid'.format(len(local_but_not_grid))
+    logger.info(msg)
+    return local_but_not_grid
+
+def diff_grid_but_not_local(entries_grid, entries_local, cache_path='/tmp'):
+    _, _, _, cache_diff_grid_local = get_cache_files(cache_path)
+    grid_but_not_local = get_diff(entries_grid, entries_local, cache_diff_grid_local)
+    msg = '{0} items on grid but not on local FS'.format(len(grid_but_not_local))
+    logger.info(msg)
+    return grid_but_not_local
+
+def check(grid_path, local_path, localfs=hdfs, cache_path='/tmp'):
+    entries_local = local_entries(local_path, localfs.ls, cache_path)
+    entries_grid = grid_entries(grid_path, grid.ls, cache_path)
+
+    local_but_not_grid = diff_local_but_not_grid(entries_grid, entries_local, cache_path)
+    
+    grid_but_not_local = diff_grid_but_not_local(entries_grid, entries_local, cache_path)
+    return grid_but_not_local, local_but_not_grid
