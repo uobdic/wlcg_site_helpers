2fb0b45ae60e7385bf3f4d97a8496d566cb9e3d1
kreczko <lkreczko@gmail.com>
2018-07-13 15:09:09 +0100

    added size and date to HDFS output

--- wlcg_site_helpers/storage/backend/hdfs.py
+++ wlcg_site_helpers/storage/backend/hdfs.py
@@ -6,7 +6,11 @@ fs = AutoConfigClient()
 def ls(paths=['/'], recursive=False):
     if not isinstance(paths, list) and not isinstance(paths, tuple):
         paths = [paths]
-    data = [dict(size=p['length'], path=p['path']) for p in fs.ls(paths, recursive)]
+    data = [dict(size=p['length'], path=p['path'], ftype=p['file_type'], date=p['modification_time']) for p in fs.ls(paths, recursive)]
     df = pd.DataFrame(data)
-    df = df.reindex(['size', 'path'], axis=1)
+    df = df.reindex(['size', 'path', 'ftype', 'date'], axis=1)
+    df['size'] = df['size'].astype(int)
+    df['date'] = pd.to_datetime(df['date'], unit='ms')
+    # ignore current uploads
+    df = df[~df.path.str.endswith('.upload')]
     return df
